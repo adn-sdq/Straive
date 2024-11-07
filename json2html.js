b76c2764eda15cb4ae9e3667229c8875a739f4cc
Adnan Siddiqui <adn.memos@gmail.com>
2024-11-07 18:42:46 +0530

    Add files via upload

---
+++ json2html.js
@@ -0,0 +1,25 @@
+export default function json2html(data) {
+  const columns = ["Name", "Age", "Gender"];
+  
+  // Start with the table and add data-user attribute
+  let html = `<table data-user="adnansmemail@gmail.com">`;
+  
+  // Generate the table header
+  html += "<thead><tr>";
+  columns.forEach(column => {
+    html += `<th>${column}</th>`;
+  });
+  html += "</tr></thead><tbody>";
+  
+  // Generate the table rows
+  data.forEach(row => {
+    html += "<tr>";
+    columns.forEach(column => {
+      html += `<td>${row[column] || ""}</td>`;
+    });
+    html += "</tr>";
+  });
+  
+  html += "</tbody></table>";
+  return html;
+}
